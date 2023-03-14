40df0a081479a7483278383ea265b762ca523120
letienne <etienne.lemee@ynov.com>
2023-03-14 10:07:01 +0100

    finished sorting with gurvan

--- server/server.go
+++ server/server.go
@@ -1,22 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"groupietrackers"
 	"html/template"
 	"net/http"
 	"strconv"
 	"strings"
 	"sync"
-	"fmt"
 )
 
-var Cards groupietrackers.Cards //* All the data 
+var Cards groupietrackers.Cards //* All the data
 
-var CardsPagination []groupietrackers.Cards	//* Use in pagination in main page
-var SortedCardsPagination []groupietrackers.Cards //* Use in pagination when search bar used 
+var CardsPagination []groupietrackers.Cards       //* Use in pagination in main page
+var SortedCardsPagination []groupietrackers.Cards //* Use in pagination when search bar used
 
-var SelectedCard int //* Use to communicate the chosen cards id , we can't use navigate like in JSX 
-var NumberOfCards int = 10 //* The number of cards in page the server start 
+var SelectedCard int       //* Use to communicate the chosen cards id , we can't use navigate like in JSX
+var NumberOfCards int = 10 //* The number of cards in page the server start
 
 var ArtistsToDisplay groupietrackers.ArtistsToDisplay //*
 
@@ -27,7 +27,7 @@ var AdminPassword string = "admin"
 
 func main() {
 	/*
-	* We call the APICall to extract artists for the main page  
+	* We call the APICall to extract artists for the main page
 	 */
 	var wg sync.WaitGroup
 
@@ -57,7 +57,9 @@ func Inisialistion() {
 	http.HandleFunc("/NbrInPageChange", NbrInPageChange)
 	http.HandleFunc("/changeMap", MapUpDate)
 	http.HandleFunc("/reloadAPI", ReloadAPI)
-	Port := "8080"                                           //We choose port 8080
+	http.HandleFunc("/sorting", SortingList)
+
+	Port := "8080"                                          //We choose port 8080
 	fmt.Println("The serveur start on port " + Port + " 🔥") //We print this when the server is online
 	fmt.Println("http://localhost:8080/")
 	http.ListenAndServe(":"+Port, nil) //We start the server
@@ -67,7 +69,7 @@ func ReloadAPI(w http.ResponseWriter, r *http.Request) {
 	/*
 	* Function who reload information from the api
 	 */
-	 groupietrackers.APICall("https://groupietrackers.herokuapp.com/api/artists", &Cards.Array)
+	groupietrackers.APICall("https://groupietrackers.herokuapp.com/api/artists", &Cards.Array)
 	FastServerStart()
 	Adminpage(w, r)
 }
@@ -80,7 +82,7 @@ func NbrInPageChange(w http.ResponseWriter, r *http.Request) {
 	if err == nil {
 		NumberOfCards = NewNumberOfCards
 		var TmpValueForCards = Cards.Array
-		CardsPagination =  groupietrackers.IntoMultiplePages(&NumberOfCards, TmpValueForCards, 1, &Cards.ForReacherchBar)
+		CardsPagination = groupietrackers.IntoMultiplePages(&NumberOfCards, TmpValueForCards, 1, &Cards.ForReacherchBar)
 		MainPage(w, r)
 	} else {
 		MainPage(w, r)
@@ -125,6 +127,7 @@ func Adminpage(w http.ResponseWriter, r *http.Request) {
 }
 
 func MainPage(w http.ResponseWriter, r *http.Request) {
+
 	/*
 	* Function who redirecte to the main page
 	 */
@@ -179,7 +182,7 @@ func searchName(w http.ResponseWriter, r *http.Request) {
 				NewDataForInput.Array = append(NewDataForInput.Array, value)
 			}
 		}
-		SortedCardsPagination =  groupietrackers.IntoMultiplePages(&NumberOfCards, NewDataForInput.Array, -1 ,&Cards.ForReacherchBar)
+		SortedCardsPagination = groupietrackers.IntoMultiplePages(&NumberOfCards, NewDataForInput.Array, -1, &Cards.ForReacherchBar)
 		tmpl := template.Must(template.ParseFiles("./template/mainPage.html")) //We link the template and the html file
 		tmpl.Execute(w, SortedCardsPagination[0])
 	}
@@ -221,38 +224,39 @@ func FastServerStart() {
 		Cards.Array[index].ConcertDates = groupietrackers.DatesEx.Index[index].Dates
 		Cards.Array[index].Relations = groupietrackers.RelationEx.Index[index].DatesLocations
 	}
-
+	SortByAlphabetical(Cards.Array)
 	var TmpValueForCards = Cards.Array
 	Cards.ForReacherchBar = Cards.Array
-	CardsPagination = groupietrackers.IntoMultiplePages(&NumberOfCards, TmpValueForCards, 1 ,&Cards.ForReacherchBar)
+	CardsPagination = groupietrackers.IntoMultiplePages(&NumberOfCards, TmpValueForCards, 1, &Cards.ForReacherchBar)
 	fmt.Println("loading API 100%")
 }
+
 func DataToFunctionnalData(IdArstist int) groupietrackers.ArtistsToDisplay {
 	/*
 	* This function is called when we clic on a artist card , we make a new struct with
 	* the data of the artist for golang template because we can't use the struct of the json file
 	 */
 	//* We call spotify api
-	groupietrackers.GetArtist(Cards.Array[SelectedCard].Name, &SpotifyToken)
+	groupietrackers.GetArtist(Cards.Array[IdArstist].Name, &SpotifyToken)
 	var ArtistsToDisplay groupietrackers.ArtistsToDisplay
-	ArtistsToDisplay.Id = Cards.Array[SelectedCard].Id
-	ArtistsToDisplay.Image = Cards.Array[SelectedCard].Image
-	ArtistsToDisplay.Name = Cards.Array[SelectedCard].Name
-	ArtistsToDisplay.CreationDate = Cards.Array[SelectedCard].CreationDate
+	ArtistsToDisplay.Id = Cards.Array[IdArstist].Id
+	ArtistsToDisplay.Image = Cards.Array[IdArstist].Image
+	ArtistsToDisplay.Name = Cards.Array[IdArstist].Name
+	ArtistsToDisplay.CreationDate = Cards.Array[IdArstist].CreationDate
 	ArtistsToDisplay.SpotifyId = groupietrackers.SpotifyInfo.Artists.Items[0].Id
 	ArtistsToDisplay.Genre = groupietrackers.SpotifyInfo.Artists.Items[0].Genres[0]
 	ArtistsToDisplay.Followers = groupietrackers.SpotifyInfo.Artists.Items[0].Followers.Total
 
 	//* We make a new struct for the members
-	for _, value := range Cards.Array[SelectedCard].Members {
+	for _, value := range Cards.Array[IdArstist].Members {
 		toAppend := new(groupietrackers.Member)
 		toAppend.Member = value
 		ArtistsToDisplay.Members = append(ArtistsToDisplay.Members, *toAppend)
 	}
 	//* We make a new struct for the concerts
 	ArtistsToDisplay.Concert = nil
-	for _, value := range Cards.Array[SelectedCard].Locations {
-		for _, date := range Cards.Array[SelectedCard].Relations[value] {
+	for _, value := range Cards.Array[IdArstist].Locations {
+		for _, date := range Cards.Array[IdArstist].Relations[value] {
 			toAppend := new(groupietrackers.Concert)
 			toAppend.Location = value
 			toAppend.Date = date
@@ -275,4 +279,75 @@ func DataToFunctionnalData(IdArstist int) groupietrackers.ArtistsToDisplay {
 		ArtistsToDisplay.Concert[index].Id = index
 	}
 	return ArtistsToDisplay
+}
+
+func SortingList(w http.ResponseWriter, r *http.Request) {
+	Entry := r.FormValue("sorting") // vas chercher sorting dans mainPage.html
+	// reverseAlphabeticalOrder := r.FormValue("sorting")
+	// numberOfArtists := r.FormValue("sorting")
+	// alphabeticalOrder := SortByAlphabetical()
+
+	if Entry == "alphabeticalOrder" {
+		SortByAlphabetical(Cards.Array)
+	} else if Entry == "reverseAlphabeticalOrder" {
+		SortByReverseAlphabetical(Cards.Array)
+	} else if Entry == "numberOfArtists" {
+		SortByNumberOfArtist(Cards.Array)
+	}
+
+	// if alphabetical order = true --> return main page
+	// fmt.Println(alphabeticalOrder)
+	// SortByReverseAlphabetical(Cards.Array)
+	MainPage(w, r)
+}
+
+func SortByAlphabetical(Entry []groupietrackers.Artists) {
+	index := 0
+	lenght := len(Entry)
+
+	for index < lenght-1 {
+		if Entry[index].Name > Entry[index+1].Name {
+			Entry[index], Entry[index+1] = Entry[index+1], Entry[index]
+			index = 0
+		} else {
+			index++
+		}
+
+	}
+	Cards.Array = Entry
+	CardsPagination = groupietrackers.IntoMultiplePages(&NumberOfCards, Entry, 1, &Cards.ForReacherchBar)
+}
+
+func SortByReverseAlphabetical(Entry []groupietrackers.Artists) {
+	index := 0
+	lenght := len(Entry)
+
+	for index < lenght-1 {
+		if Entry[index].Name < Entry[index+1].Name {
+			Entry[index], Entry[index+1] = Entry[index+1], Entry[index]
+			index = 0
+		} else {
+			index++
+		}
+
+	}
+	Cards.Array = Entry
+	CardsPagination = groupietrackers.IntoMultiplePages(&NumberOfCards, Entry, 1, &Cards.ForReacherchBar)
+}
+
+func SortByNumberOfArtist(Entry []groupietrackers.Artists) {
+	index := 0
+	lenght := len(Entry) - 1
+
+	for index < lenght {
+		if len(Entry[index].Members) > len(Entry[index+1].Members) {
+			Entry[index], Entry[index+1] = Entry[index+1], Entry[index]
+			index = 0
+		} else {
+			index++
+		}
+	}
+
+	Cards.Array = Entry
+	CardsPagination = groupietrackers.IntoMultiplePages(&NumberOfCards, Entry, 1, &Cards.ForReacherchBar)
 }
